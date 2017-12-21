b5efda4e23f9a51cb93e4f0ab16ab7807eb9a9ea
lulei11 <lulei@jd.com>
2017-12-21 16:31:20 +0800

    init

--- src/main/scala/sql/demo/CSVDemo.scala
+++ src/main/scala/sql/demo/CSVDemo.scala
@@ -7,15 +7,46 @@ import org.apache.spark.sql.SparkSession
   * spark-submit --class "sql.demo.CSVDemo" --master local[*] sparkapp_2.11-1.0.jar
   */
 object CSVDemo {
+  case class Employee(id:Int,name:String)
+
   def main(args: Array[String]): Unit = {
     // sparkContext
     val sparkSession = SparkSession.builder.master("local[*]")
       .appName("CSVDemo")
       .getOrCreate()
 
-    val df = sparkSession.read.option("header","true").
+
+
+    val listEmployee = List(Employee(1, "iteblog"), Employee(2, "Jason"), Employee(3, "Abhi"))
+    val empFrame = sparkSession.createDataFrame(listEmployee);
+    empFrame.printSchema()
+    empFrame.registerTempTable("emp")
+
+    val emps = sparkSession.sql("select * from emp order by name desc")
+    emps.show()
+
+
+
+    val student = sparkSession.read.option("header","true").
       csv("data/student.csv")
-    df.show()
+    student.show()
+
+    student.head(5).foreach(println)
+
+    print(student.first())
+
+    val emailFrame = student.select("email")
+
+    emailFrame.show(3)
+
+    val studentEmailFrame = student.select("studentName","email");
+    studentEmailFrame.show(5)
+
+
+
+
+
+
 
   }
 }
