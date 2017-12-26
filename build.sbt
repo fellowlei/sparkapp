2e602d9922c82eeb0964135c0ec4bf8feb4c3428
lulei11 <lulei@jd.com>
2017-12-26 16:22:02 +0800

    init

--- build.sbt
+++ build.sbt
@@ -6,11 +6,11 @@ scalaVersion := "2.11.11"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.2.0"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0" % "provided"
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming-kafka-0-10
-libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.2.0" % Test
+libraryDependencies += "org.apache.spark" %% "spark-streaming-kafka-0-10" % "2.2.0"
 // https://mvnrepository.com/artifact/mysql/mysql-connector-java
 libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.40"
 
--- src/main/scala/kafka/LogStream.scala
+++ src/main/scala/kafka/LogStream.scala
@@ -1,17 +1,41 @@
 package kafka
 
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Created by lulei on 2017/12/19.
+  * spark-submit --class "kafka.LogStream" --master local[*] sparkapp_2.11-1.0.jar
   */
 object LogStream {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("LogStream").setMaster("local[*]")
     val sc =new SparkContext(conf)
-    //每60秒一个批次
-    val ssc =new StreamingContext(sc,Seconds(60))
+    //每5秒一个批次
+    val ssc =new StreamingContext(sc,Seconds(5))
+
+    val topic = Array("test")
+
+    val group = "con-consumer-group"
+//    KafkaUtils
+    val kafkaParam = Map(
+      "bootstrap.servers" ->"172.28.5.2:9092",
+      "group.id" -> group,
+      //如果没有初始化偏移量或者当前的偏移量不存在任何服务器上，可以使用这个配置属性
+      //可以使用这个配置，latest自动重置偏移量为最新的偏移量
+      "auto.offset.reset" -> "latest",
+      //如果是true，则这个消费者的偏移量会在后台自动提交
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    //创建DStream，返回接收到的输入数据
+    var stream=KafkaUtils.createDirectStream[String,String](ssc,LocationStrategies.PreferConsistent,ConsumerStrategies.Subscribe[String,String](topic,kafkaParam))
+    stream.map(s =>(s.key(),s.value())).print();
+    ssc.start();
+    ssc.awaitTermination();
+
+
 
   }
 }
