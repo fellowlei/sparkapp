2413845d746d932d5e51d266cd2f4e651e19a0aa
lulei11 <lulei@jd.com>
2017-12-26 16:26:52 +0800

    init

--- src/main/scala/kafka/LogStream.scala
+++ src/main/scala/kafka/LogStream.scala
@@ -1,5 +1,6 @@
 package kafka
 
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
@@ -21,6 +22,8 @@ object LogStream {
 //    KafkaUtils
     val kafkaParam = Map(
       "bootstrap.servers" ->"172.28.5.2:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
       "group.id" -> group,
       //如果没有初始化偏移量或者当前的偏移量不存在任何服务器上，可以使用这个配置属性
       //可以使用这个配置，latest自动重置偏移量为最新的偏移量
